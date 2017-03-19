c4fcebaa9cc189d58d6723bb0e7059f405f44842
ayushr2 <ayushr2@illinois.edu>
2017-03-19 06:43:56 -0700

    entities

--- watson_responses.py
+++ watson_responses.py
@@ -10,6 +10,10 @@ class watson:
             password= config.watson_password,
             version='2017-02-03')
         self.our_context = dict()
+        self.place = ""
+        self.transport = ""
+        self.accomodation = ""
+        self.activities = ""
     def get_response(self,bot_response):
         response = self.conversation.message(workspace_id=self.workspace_id, message_input={
         'text': bot_response},
@@ -17,10 +21,26 @@ class watson:
         json_string = json.dumps(response, indent=2)
         #print(json_string)
 
+        #get entities
+        entities = response["entities"]
+        if len(entities) > 0:
+            entity = entities[0]
+            if entity["entity"] == "transportation":
+                self.transport = entity["value"]
+            elif entity["entity"] == "place":
+                self.place = entity["value"]
+            elif entity["entity"] == "housing":
+                self.place = entity["value"]
+            elif entity["entity"] == "activities":
+                self.activities = entity["value"]
+
+                pass
+            pass
+
         #global our_context
         self.our_context = response["context"]
         parsed_json = json.loads(json_string)
         if len(parsed_json["output"]["text"])!= 0:
             return parsed_json["output"]["text"][0]
-        return ""
+        return "I am sorry I did not understand that."
         #print self.our_context
